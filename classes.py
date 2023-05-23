9433125e9d07c735f6abffef046b05ac8e40212d
Wiktor Kycia <wiktor.kycia@gmail.com>
2023-05-23 19:11:53 +0200

    get width and height of the image

--- classes.py
+++ classes.py
@@ -20,6 +20,8 @@ class PlayableShip:
     def __init__(self, game, image_path):
         self.game = game
         self.image = pygame.image.load(os.path.join(image_path))
+        self.width = self.image.get_width()
+        self.height = self.image.get_height()
 
 class Scout:
     def __init__(self, x, y, game):
