89137136e3b2420f0692cbceb76b10261e3c0080
Wiktor Kycia <wiktor.kycia@gmail.com>
2023-05-24 18:02:42 +0200

    add if expression in tick method it checks if the player pressed space button on keyboard

--- classes.py
+++ classes.py
@@ -55,6 +55,8 @@ class PlayableShip(object):
         self.pos += self.vel
         self.acc *= 0
 
+        if pressed[pygame.K_SPACE]:
+            pass
     def draw(self):
         self.game.screen.blit(self.image, (self.pos.x - self.width / 2, self.pos.y - self.height / 2))
 
