1779f29fb5c9c125806fcbe36eca24f9e9c9f483
Wiktor Kycia <wiktor.kycia@gmail.com>
2023-05-24 17:44:10 +0200

    add hitbox and define the default color to white

--- classes.py
+++ classes.py
@@ -85,9 +85,10 @@ class Scout(PlayableShip):
         # pygame.draw.polygon(self.game.screen, (255, 255, 255), self.points)
 
 class Bullet(object):
-    def __init__(self, x, y, width, height, speed, force, mass, color):
+    def __init__(self, x, y, width, height, speed, force, mass, color=(255, 255, 255)):
         self.pos = Vector2(x, y)
         # self.vel = speed
+        self.hitbox = pygame.Rect(x - width / 2, y - height / 2, width, height)
     def tick(self):
         pass
     def draw(self):
