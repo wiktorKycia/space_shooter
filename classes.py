18c34524c949b88b2db9ee162b85935a871148b0
Wiktor Kycia <wiktor.kycia@gmail.com>
2023-05-28 19:28:45 +0200

    add mass atribute to bullet class group the parameter inside the constructor

--- classes.py
+++ classes.py
@@ -113,12 +113,14 @@ class Bullet(object):
     def __init__(self, game, x, y, width, height, force, mass, color=(255, 255, 255)):
         self.pos = Vector2(x, y)
         self.vel = Vector2(0, 0)
-        self.hitbox = pygame.Rect(self.pos.x - width / 2, self.pos.y - height / 2, width, height)
+
+        self.mass = mass
         acc = int(force / mass)
         self.acc = Vector2(0, -acc)
 
         self.width = width
         self.height = height
+        self.hitbox = pygame.Rect(self.pos.x - width / 2, self.pos.y - height / 2, width, height)
 
         self.game = game
         self.color = color
