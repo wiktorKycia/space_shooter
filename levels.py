6bd235c1f6f666677d3c774d8a44397b514c7051
Wiktor Kycia <wiktor.kycia@gmail.com>
2023-06-13 21:22:10 +0200

    planning changes

--- levels.py
+++ levels.py
@@ -1,1 +1,9 @@
 from enemies import *
+# klasa minilevel
+# jako argument przyjmuje listę wszystkich przeciwników
+# poszczególnymi metodami generuje zestawy przeciwników, lub pojedyńczo
+#
+# klasa level
+# zawiera wywołania poszczególnych metod z minilevel'a
+#
+# lista przeciwników w mainie
