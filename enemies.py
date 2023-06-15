0096320067fb217147a19bda7c8a66fd1c78fd08
Wiktor Kycia <wiktor.kycia@gmail.com>
2023-06-15 19:48:52 +0200

    add basic parameters such as x, y, isshooting, movforce, mass and shotforce x, y define the initial position isshooting define whether the enemy will be able to shoot movforce and mass will be useful with moving shotforce define the force which is used to shoot a bullet

--- enemies.py
+++ enemies.py
@@ -1,4 +1,9 @@
 from bullets import *
 class BaseEnemy(object):
-    def __init__(self):
-        pass
+    def __init__(self, x, y, isshooting, movforce, mass, shotforce):
+        self.x = x
+        self.y = y
+        self.isshooting = isshooting
+        self.movforce = movforce
+        self.mass = mass
+        self.shotforce = shotforce
