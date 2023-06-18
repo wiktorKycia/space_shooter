db172f0194a12d5cec4fdae7ef587c8531ab6b53
Wiktor Kycia <wiktor.kycia@gmail.com>
2023-06-18 18:47:01 +0200

    visible the mouse, check mask overlap for player and mouse

--- ships.py
+++ ships.py
@@ -12,7 +12,7 @@ class PlayableShip(object):
         self.image = pygame.image.load(os.path.join(image_path)).convert_alpha()
         self.hitbox = self.image.get_rect()
         self.mask = pygame.mask.from_surface(self.image)
-
+        # self.mask.set_at()
         self.width = self.image.get_width()
         self.height = self.image.get_height()
 
