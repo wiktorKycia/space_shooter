ca33433b8478d5d70ecf79b9b6865d4695ddd1d0
Wiktor Kycia <wiktor.kycia@gmail.com>
2023-06-18 18:16:01 +0200

    comment hitbox in bullets

--- bullets.py
+++ bullets.py
@@ -14,7 +14,7 @@ class Bullet(object):
 
         self.width = width
         self.height = height
-        self.hitbox = pygame.Rect(self.pos.x - width / 2, self.pos.y - height / 2, width, height)
+        # self.hitbox = pygame.Rect(self.pos.x - width / 2, self.pos.y - height / 2, width, height)
 
         self.mask = pygame.mask.from_surface(self.hitbox)
 
