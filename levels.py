96694c06c21824953598213aef6325f7365de44c
Wiktor Kycia <wiktor.kycia@gmail.com>
2023-06-18 17:11:48 +0200

    delete things from todo list in levels.py

--- levels.py
+++ levels.py
@@ -1,12 +1,6 @@
 from enemies import *
-# klasa minilevel
-# jako argument przyjmuje listę wszystkich przeciwników
-# poszczególnymi metodami generuje zestawy przeciwników, lub pojedyńczo
-#
 # klasa level
 # zawiera wywołania poszczególnych metod z minilevel'a
-#
-# lista przeciwników w mainie
 class Minilevel():
     def __init__(self, game):
         self.game = game
