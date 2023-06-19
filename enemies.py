1d1db9146a992c97749560a8067adc9ae5ba850c
Wiktor Kycia <wiktor.kycia@gmail.com>
2023-06-19 18:25:56 +0200

    set all the enemy hp to 100

--- enemies.py
+++ enemies.py
@@ -58,7 +58,7 @@ class Enemy1(BaseEnemy):
     def __init__(self, game, x, y):
         self.game = game
         self.image = "./enemies/Enemy1.png"
-        super().__init__(self.game, self.image, x, y , 0.99, 1000, 500, 8000, 50)
+        super().__init__(self.game, self.image, x, y , 0.99, 1000, 500, 8000, 50, 100)
 
     def add_force(self, force):
         super().add_force(force)
@@ -91,7 +91,7 @@ class Enemy2(BaseEnemy):
     def __init__(self, game, x, y):
         self.game = game
         self.image = "./enemies/Enemy2.png"
-        super().__init__(self.game, self.image, x, y , 0.99, 1500, 1000, 12000, 50)
+        super().__init__(self.game, self.image, x, y , 0.99, 1500, 1000, 12000, 50, 100)
 
     def add_force(self, force):
         super().add_force(force)
@@ -124,7 +124,7 @@ class Enemy3(BaseEnemy):
     def __init__(self, game, x, y):
         self.game = game
         self.image = "./enemies/Enemy3.png"
-        super().__init__(self.game, self.image, x, y , 0.98, 5400, 3500, 25000, 60)
+        super().__init__(self.game, self.image, x, y , 0.98, 5400, 3500, 25000, 60, 100)
 
     def add_force(self, force):
         super().add_force(force)
