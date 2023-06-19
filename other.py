75d4f0f718b71bb5af7b289ebb66dc7e88bab785
Wiktor Kycia <wiktor.kycia@gmail.com>
2023-06-19 17:07:56 +0200

    add the most important parameter amount

--- other.py
+++ other.py
@@ -1,8 +1,9 @@
 import pygame
 
 class HP:
-    def __init__(self, game, width, height, x, y, color=(250, 250, 250)):
+    def __init__(self, game, amount, width, height, x, y, color=(250, 250, 250)):
         self.game = game
+        self.amount = amount
 
         self.x = x
         self.y = y
