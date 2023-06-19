fe7d7559cf187fdf703e5702cd3052fd7242844c
Wiktor Kycia <wiktor.kycia@gmail.com>
2023-06-19 17:05:46 +0200

    define basic parameters

--- other.py
+++ other.py
@@ -1,4 +1,11 @@
 import pygame
 
 class HP:
-    def __init__(self):
+    def __init__(self, game, width, height, x, y):
+        self.game = game
+
+        self.x = x
+        self.y = y
+
+        self.width = width
+        self.height = height
