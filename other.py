bccd53bfafe974d5171e49a301c8d20feee9e1d5
Wiktor Kycia <wiktor.kycia@gmail.com>
2023-06-19 17:12:01 +0200

    add full parameter which is also initially equal to amount

--- other.py
+++ other.py
@@ -3,6 +3,7 @@ import pygame
 class HP:
     def __init__(self, game, amount, width, height, x, y, color=(250, 250, 250)):
         self.game = game
+        self.full = amount
         self.amount = amount
 
         self.x = x
