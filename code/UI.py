205aeeb018ac154ce69110441d50f8986202927c
Wiktor Kycia <wiktor.kycia@gmail.com>
2023-06-26 12:32:11 +0200

    add scale argument to the init method

--- code/UI.py
+++ code/UI.py
@@ -1,7 +1,7 @@
 import pygame
 
 class Button():
-    def __init__(self, game, x, y, image):
+    def __init__(self, game, x, y, image, scale):
         self.game = game
         self.x = x
         self.y = y
