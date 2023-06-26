baa881f767c6c2cd5a78bed793d93045bea95f02
Wiktor Kycia <wiktor.kycia@gmail.com>
2023-06-26 12:43:25 +0200

    add clicked parameter and add to the second if, check if it is not clicked

--- code/UI.py
+++ code/UI.py
@@ -8,10 +8,12 @@ class Button():
         self.image = pygame.transform.scale(image, (int(width * scale), int(height * scale)))
         self.rect = self.image.get_rect()
         self.rect.center = (x, y)
+        self.clicked = False
 
     def draw(self, surface):
         pos = pygame.mouse.get_pos()
         # check if the rect collides with the mouse
         if self.rect.collidepoint(pos):
             # check if the mouse is clicked
-            if pygame.mouse.get_pressed()[0] == 1:
+            if pygame.mouse.get_pressed()[0] == 1 and self.clicked == False:
+                self.clicked = True
