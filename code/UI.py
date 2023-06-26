945e9e2b7f62cf81a0eb5ac7a4194968e5c01a9c
Wiktor Kycia <wiktor.kycia@gmail.com>
2023-06-26 12:47:41 +0200

    change width and height to a parameters

--- code/UI.py
+++ code/UI.py
@@ -3,8 +3,8 @@ import pygame
 class Button():
     def __init__(self, game, x:int, y:int, image:pygame.image, scale:float = 1.0):
         self.game = game
-        width = image.get_width()
-        height = image.get_height()
+        self.width = image.get_width()
+        self.height = image.get_height()
         self.image = pygame.transform.scale(image, (int(width * scale), int(height * scale)))
         self.rect = self.image.get_rect()
         self.rect.center = (x, y)
