d9d6d7fed73b7df39e3ee502928f6dabb0684b08
Wiktor Kycia <wiktor.kycia@gmail.com>
2023-06-26 12:36:45 +0200

    delete x, y parameters, get the rect of an image

--- code/UI.py
+++ code/UI.py
@@ -3,8 +3,8 @@ import pygame
 class Button():
     def __init__(self, game, x:int, y:int, image:pygame.image, scale:float = 1.0):
         self.game = game
-        self.x = x
-        self.y = y
         width = image.get_width()
         height = image.get_height()
         self.image = pygame.transform.scale(image, (int(width * scale), int(height * scale)))
+        self.rect = self.image.get_rect()
+        self.rect.center = (x, y)
