c5cd6e0eaf95af277affdcd7ec45f35fff0d2a53
Wiktor Kycia <wiktor.kycia@gmail.com>
2023-06-26 12:49:53 +0200

    bring back x, y parameters and correct the image transform

--- code/UI.py
+++ code/UI.py
@@ -3,9 +3,11 @@ import pygame
 class Button():
     def __init__(self, game, x:int, y:int, image:pygame.image, scale:float = 1.0):
         self.game = game
+        self.x = x
+        self.y = y
         self.width = image.get_width()
         self.height = image.get_height()
-        self.image = pygame.transform.scale(image, (int(width * scale), int(height * scale)))
+        self.image = pygame.transform.scale(image, (int(self.width * scale), int(self.height * scale)))
         self.rect = self.image.get_rect()
         self.rect.center = (x, y)
         self.clicked = False
@@ -19,3 +21,4 @@ class Button():
                 self.clicked = True
         if pygame.mouse.get_pressed()[0] == 0:
             self.clicked = False
+        surface.blit(self.image, ())
