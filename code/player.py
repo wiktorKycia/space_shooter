eccc961f046fd241d306213f343b05a31fbee375
Wiktor Kycia <wiktor.kycia@gmail.com>
2023-06-27 13:47:04 +0200

    add list called ships, that will store all the player's ships

--- code/player.py
+++ code/player.py
@@ -7,6 +7,7 @@ class Player(object):
     def __init__(self, game, coins=1500):
         self.game = game
         self.coins = coins
+        self.ships = []
 
     def add_coins(self, amount:int):
         self.coins += amount
