ae7f79633771f763391c7a9c9b089312a9f486cd
Wiktor Kycia <wiktor.kycia@gmail.com>
2023-06-27 13:42:13 +0200

    create add_coins method that adds given amount of coins

--- code/player.py
+++ code/player.py
@@ -7,3 +7,6 @@ class Player(object):
     def __init__(self, game, coins=1500):
         self.game = game
         self.coins = coins
+
+    def add_coins(self, amount:int):
+        self.coins += amount
