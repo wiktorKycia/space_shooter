aadd538740d3581472c97cc82b77fa249d4ffe8c
Wiktor Kycia <wiktor.kycia@gmail.com>
2023-06-27 11:59:23 +0200

    move the code to package code

---
+++ code/other.py
@@ -0,0 +1,105 @@
+import pygame
+
+class HP:
+    def __init__(self, game, amount, width, height, x, y, color=(250, 250, 250)):
+        self.game = game
+        self.amount = amount
+
+        self.x = x
+        self.y = y
+
+        self.width = width
+        self.height = height
+
+        self.color = color
+        self.bgcolor = (255, 0, 0)#(color[0] - 100, color[1] - 100, color[2] - 100)
+
+        self.back = pygame.Rect(self.x - self.width/2, self.y - self.height/2, self.width, self.height)
+
+        self.unit = self.width / self.amount
+
+        self.block = pygame.Rect(self.x - self.width/2, self.y - self.height/2, self.unit * self.amount, self.height)
+
+    def decrease_by(self, amount):
+        self.amount -= amount
+        self.tick()
+        self.draw()
+
+    def tick(self):
+        self.block = pygame.Rect(self.x - self.width/2, self.y - self.height/2, self.unit * self.amount, self.height)
+
+    def draw(self):
+        # self.game.screen.blit(self.surf, (self.x - self.width/2, self.y - self.height/2))
+        pygame.draw.rect(self.game.screen, self.bgcolor, self.back)
+        pygame.draw.rect(self.game.screen, self.color, self.block)
+
+class DeluxeHP:
+    def __init__(self, game, amount, x, y, width, height, color=(250, 0, 0)):
+        self.game = game
+        self.current_hp = amount
+        self.max_hp = amount
+        self.hp = amount
+
+        self.bar_length = width
+        self.health_ratio = self.max_hp / self.bar_length
+        self.change_speed = 2000
+
+        self.height = height
+        self.x = x
+        self.y = y
+        self.color = color
+
+    def get_damage(self, amount):
+        if self.hp > 0:
+            self.hp -= amount
+        if self.hp < 0:
+            self.hp = 0
+
+    def get_health(self, amount):
+        if self.hp < self.max_hp:
+            self.hp += amount
+        if self.hp > self.max_hp:
+            self.hp = self.max_hp
+
+    def tick(self):
+        transition_width = 0
+        transition_color = (255, 0, 0)
+
+        if self.current_hp < self.hp:
+            self.current_hp += self.change_speed
+            if self.current_hp > self.hp: self.current_hp = self.hp
+            transition_width = int((self.hp - self.current_hp) / self.health_ratio)
+            transition_color = (0, 255, 0)
+
+            health_bar_width = int(self.current_hp / self.health_ratio)
+            health_bar = pygame.Rect(self.x, self.y, health_bar_width, self.height)
+            transition_bar = pygame.Rect(health_bar.right, self.y, transition_width, self.height)
+
+            pygame.draw.rect(self.game.screen, self.color, health_bar)
+            pygame.draw.rect(self.game.screen, transition_color, transition_bar)
+            pygame.draw.rect(self.game.screen, (255, 255, 255), (self.x, self.y, self.bar_length, self.height), 2)
+
+
+        elif self.current_hp > self.hp:
+            self.current_hp -= self.change_speed
+            if self.current_hp < self.hp: self.current_hp = self.hp
+            transition_width = int((self.current_hp - self.hp) / self.health_ratio)
+            transition_color = (255, 255, 0)
+
+            health_bar_width = int(self.hp / self.health_ratio)
+            health_bar = pygame.Rect(self.x, self.y, health_bar_width, self.height)
+            transition_bar = pygame.Rect(health_bar.right, self.y, transition_width, self.height)
+
+            pygame.draw.rect(self.game.screen, self.color, health_bar)
+            pygame.draw.rect(self.game.screen, transition_color, transition_bar)
+            pygame.draw.rect(self.game.screen, (255, 255, 255), (self.x, self.y, self.bar_length, self.height), 2)
+
+
+        else:
+            health_bar_width = int(self.current_hp / self.health_ratio)
+            health_bar = pygame.Rect(self.x, self.y, health_bar_width, self.height)
+            # transition_bar = pygame.Rect(health_bar.right, self.y, transition_width, self.height)
+
+            pygame.draw.rect(self.game.screen, self.color, health_bar)
+            # pygame.draw.rect(self.game.screen, transition_color, transition_bar)
+            pygame.draw.rect(self.game.screen, (255, 255, 255), (self.x, self.y, self.bar_length, self.height), 2)
---
+++ code/ships.py
@@ -0,0 +1,179 @@
+import pygame
+from pygame import mixer
+from pygame.math import Vector2
+from code.other import *
+import os
+from code.bullets import *
+# from bullets import *
+mixer.init()
+
+class PlayableShip(object):
+    def __init__(self, game, image_path, slip, mov_force, mass, shot_force,barrel_lenght):
+        self.game = game
+        self.image = pygame.image.load(os.path.join(image_path)).convert_alpha()
+        self.hitbox = self.image.get_rect()
+        self.mask = pygame.mask.from_surface(self.image)
+        # self.mask.set_at()
+        self.width = self.image.get_width()
+        self.height = self.image.get_height()
+
+        self.slip = slip
+        self.mov_force = mov_force
+        self.mass = mass
+
+        size = self.game.screen.get_size()
+        self.pos = Vector2(size[0] / 2, size[1] / 2)
+        self.vel = Vector2(0, 0)
+        self.acc = Vector2(0, 0)
+
+        self.shot_force = shot_force
+        self.barrel = barrel_lenght
+        self.bullets = []
+        self.clock = 0
+
+        self.hp = DeluxeHP(self.game, 1000000, 10, 700, 350, 30)
+
+    def add_force(self, force):
+        self.acc += force / self.mass
+
+    def tick(self):
+        # Input
+        pressed = pygame.key.get_pressed()
+        if pressed[pygame.K_w]:
+            self.add_force(Vector2(0, -self.mov_force))
+        if pressed[pygame.K_s]:
+            self.add_force(Vector2(0, self.mov_force))
+        if pressed[pygame.K_d]:
+            self.add_force(Vector2(self.mov_force, 0))
+        if pressed[pygame.K_a]:
+            self.add_force(Vector2(-self.mov_force, 0))
+
+        # Physics
+        self.vel *= self.slip
+        self.vel -= Vector2(0, 0)
+
+        self.vel += self.acc
+        self.pos += self.vel * self.game.dt
+        self.acc *= 0
+
+        self.hitbox.center = (self.pos.x, self.pos.y)
+
+        self.clock += self.game.dt
+
+        for bullet in self.bullets:
+            if bullet.pos.y <= -bullet.height:
+                self.bullets.remove(bullet)
+            else:
+                bullet.tick()
+        self.hp.tick()
+    def draw(self):
+        for bullet in self.bullets:
+            bullet.draw()
+            if bullet.pos.y <= 0 - bullet.height:
+                self.bullets.remove(bullet)
+        self.game.screen.blit(self.image, (self.pos.x - self.width / 2, self.pos.y - self.height / 2))
+        pygame.draw.rect(self.game.screen, (255, 255, 255), self.hitbox, 1)
+        # self.hp.draw()
+
+class Scout(PlayableShip):
+    def __init__(self, game):
+        self.game = game
+        self.path = "./ships/statek1.png"
+        super().__init__(self.game, self.path, 0.99, 1000, 2000, 1500, 10)
+        # # self.points = [Vector2(0, -26), Vector2(20, 12), Vector2(0, 24), Vector2(-20, 12)]
+    def add_force(self, force):
+        super().add_force(force)
+    def tick(self):
+        super().tick()
+        pressed = pygame.key.get_pressed()
+        if pressed[pygame.K_SPACE] and self.clock >= 0.15:
+            self.clock = 0
+            bullet = Bullet(self.game, self.pos.x, self.pos.y, 2, 10, self.shot_force, 20, (230, 0, 0),
+                            './shot_sounds/blaster.mp3')
+            self.bullets.append(bullet)
+            bullet.sound.play(0, 800)
+            acc = -bullet.acc  # getting initial bullet velocity
+            vel = (bullet.mass * acc) / self.mass
+            # getting initial velocity from zasada zachowania pędu
+            vel.x *= vel.x
+            vel.y *= vel.y
+            energy = (self.mass * vel) / 2  # calculating kinetic energy
+            force = energy / self.barrel  # calculating kickback force
+            self.add_force(Vector2(force.x, force.y))
+    def draw(self):
+        super().draw()
+        # #Base polygon
+        # self.points = [Vector2(0, -26), Vector2(20, 12), Vector2(0, 24), Vector2(-20, 12)]
+        #
+        # #Rotate polygon
+        # angle = self.vel.angle_to(Vector2(0, 1)) # kąt między wektorem prędkości, a linią poziomą
+        # self.points = [p.rotate(angle) for p in self.points]
+        #
+        # # Fix y axis
+        # self.points = [Vector2(p.x, p.y * -1) for p in self.points]
+        #
+        # #Add current position
+        # self.points = [self.pos + p for p in self.points]
+        #
+        # #draw polygon
+        # pygame.draw.polygon(self.game.screen, (255, 255, 255), self.points)
+
+class Ship1(PlayableShip):
+    def __init__(self, game):
+        self.game = game
+        self.path = "./ships/ship1.png"
+        super().__init__(game, self.path, 0.98, 186, 40, 400, 10)
+
+    def add_force(self, force):
+        super().add_force(force)
+    def tick(self):
+        super().tick()
+        pressed = pygame.key.get_pressed()
+        if pressed[pygame.K_SPACE] and self.clock >= 0.25:
+            self.clock = 0
+            bullet = Bullet(self.game, self.pos.x, self.pos.y, 5, 50, self.shot_force, 0.5, (0, 200, 230), './shot_sounds/blaster.mp3')
+            # bullet = Kinetic60Bullet(self.game, self.pos.x, self.pos.y, self.shot_force)
+            self.bullets.append(bullet)
+            bullet.sound.play(0, 800)
+            acc = -bullet.acc # getting initial bullet velocity
+            vel = (bullet.mass * acc) / self.mass
+                # getting initial velocity from zasada zachowania pędu
+            vel.x *= vel.x
+            vel.y *= vel.y
+            energy = (self.mass * vel) / 2 # calculating kinetic energy
+            force = energy / self.barrel # calculating kickback force
+            self.add_force(Vector2(force.x, force.y))
+    def draw(self):
+        super().draw()
+
+class Ship2(PlayableShip):
+    def __init__(self, game):
+        self.game = game
+        self.path = "./ships/ship2.png"
+        super().__init__(game, self.path, 0.98, 250, 38, 500, 10)
+
+    def add_force(self, force):
+        super().add_force(force)
+    def tick(self):
+        super().tick()
+        pressed = pygame.key.get_pressed()
+        if pressed[pygame.K_SPACE] and self.clock >= 0.30:
+            self.clock = 0
+            bullet = Bullet(self.game, self.pos.x-27, self.pos.y-15, 5, 50, self.shot_force, 0.5, (0, 200, 230), './shot_sounds/blaster.mp3')
+            bullet1 = Bullet(self.game, self.pos.x+27, self.pos.y-15, 5, 50, self.shot_force, 0.5, (0, 200, 230), './shot_sounds/blaster.mp3')
+            self.bullets.append(bullet)
+            self.bullets.append(bullet1)
+            bullet.sound.play(0, 650)
+            bullet1.sound.play(0, 650)
+            acc = -bullet.acc # getting initial bullet velocity
+            vel = (bullet.mass * acc) / self.mass
+                # getting initial velocity from zasada zachowania pędu
+            vel.x *= vel.x
+            vel.y *= vel.y
+            energy = (self.mass * vel) / 2 # calculating kinetic energy
+            force = energy / self.barrel # calculating kickback force
+            self.add_force(Vector2(force.x, force.y))
+            self.add_force(Vector2(force.x, force.y))
+    def draw(self):
+        super().draw()
+
