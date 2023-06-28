78e3a3f3535a4ac0f8cc8b1723ea5080c4341835
Wiktor Kycia <wiktor.kycia@gmail.com>
2023-06-28 10:13:57 +0200

    copy code from the write function, change game to surface

--- code/UI.py
+++ code/UI.py
@@ -12,6 +12,12 @@ def write(game, text, x, y, font_size, color=(0, 0, 0), font_style="Arial", is_c
     game.screen.blit(rend, (x, y))
 
 def write_on_surface(surface, text, x, y, font_size, color=(0, 0, 0), is_centered=False, font_style='Arial'):
+    font = pygame.font.SysFont(font_style, font_size)
+    rend = font.render(text, True, color)
+    if is_centered is True:
+        x = (surface.width - rend.get_rect().width) / 2
+        y = (surface.height - rend.get_rect().height) / 2
+    surface.blit(rend, (x, y))
 
 class NoImageButton:
     def __init__(self, game, x, y, width, height):
