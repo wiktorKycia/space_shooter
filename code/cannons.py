b05cda7b9c1d18e716a815c9b57b34dbe27d7b71
Wiktor Kycia <wiktor.kycia@gmail.com>
2023-06-29 16:21:51 +0200

    define an init method with x, y, force, interval and key parameters

--- code/cannons.py
+++ code/cannons.py
@@ -1,3 +1,9 @@
 from code.bullets import *
 
 class Kinetic60Gun:
+    def __init__(self, x, y, force, interval, key):
+        self.x = x
+        self.y = y
+        self.force = force
+        self.interval = interval
+        self.key = key
