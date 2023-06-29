08fadd6eafa2e00717cc9a1d3fcfeee2901faa8c
Wiktor Kycia <wiktor.kycia@gmail.com>
2023-06-29 11:31:42 +0200

    Revert "center hp bar position in Enemy3"
    This reverts commit bf972f9db51e9aaba66902e09952b0ae96727e51.

--- code/enemies.py
+++ code/enemies.py
@@ -31,7 +31,7 @@ class BaseEnemy(object):
         self.clock = 0
         self.bullets = []
 
-        self.hp = DeluxeHP(self.game, health, self.pos.x, self.pos.y-50, 50, 10)
+        self.hp = DeluxeHP(self.game, health, self.pos.x - self.width/2, self.pos.y-50, 50, 10)
 
     def add_force(self, force):
         self.acc += force / self.mass
