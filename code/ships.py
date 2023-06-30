80f6e291f8666c5e506c8a7dc266f0ad5303f233
Wiktor Kycia <wiktor.kycia@gmail.com>
2023-06-30 12:55:30 +0200

    create cannons instances after super init

--- code/ships.py
+++ code/ships.py
@@ -110,8 +110,8 @@ class Ship1(PlayableShip):
         self.game = game
         self.path = "./ships/ship1.png"
         self.hp = DeluxeHP(self.game, 1000000, 200, 700, 350, 30)
-        self.cannon = Blaster(self.game, self, Vector2(0, -20), self.force, 0.3)
         super().__init__(game, self.path, 0.98, 150, 100, 1000)
+        self.cannon = Blaster(self.game, self, Vector2(0, -20), self.force, 0.3)
 
     def add_force(self, force):
         super().add_force(force)
@@ -127,9 +127,9 @@ class Ship2(PlayableShip):
         self.game = game
         self.path = "./ships/ship2.png"
         self.hp = DeluxeHP(self.game, 1000000, 200, 700, 350, 30)
+        super().__init__(game, self.path, 0.98, 170, 38, 500)
         self.cannon = Blaster(self.game, self, Vector2(27, -20), self.force, 0.3)
         self.cannon2 = Blaster(self.game, self, Vector2(-27, -20), self.force, 0.3)
-        super().__init__(game, self.path, 0.98, 170, 38, 500)
 
     def add_force(self, force):
         super().add_force(force)
