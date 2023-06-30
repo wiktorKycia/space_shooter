7c32988fcf2683bf9e787d7bef0378f29381515d
Wiktor Kycia <wiktor.kycia@gmail.com>
2023-06-30 12:25:08 +0200

    add KineticGun, copy from Kinetic9Gun

--- code/cannons.py
+++ code/cannons.py
@@ -76,3 +76,14 @@ class Kinetic9Gun(BaseCannon):
         bullet.sound.play(0, 800)
         self.ship.bullets.append(bullet)
         self.ship.add_force(self.calculate_kickback_force(bullet))
+
+class KineticGun(BaseCannon):
+    def __init__(self, game, ship, translation:Vector2, force:int, interval:float, key=pygame.K_SPACE):
+        self.barrel = 60
+        super().__init__(game, ship, translation, force, interval, self.barrel, key)
+
+    def shot(self):
+        bullet = KineticBullet(self.game, self.pos.x, self.pos.y, self.ship.force)
+        bullet.sound.play(0, 800)
+        self.ship.bullets.append(bullet)
+        self.ship.add_force(self.calculate_kickback_force(bullet))
