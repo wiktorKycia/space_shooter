161ceca7099e69a1550c7b3bd8b21c64d8756e38
Wiktor Kycia <wiktor.kycia@gmail.com>
2023-07-02 14:29:20 +0200

    change Ship4 parameters health 3 500 000 max speed 100 mass 500 force 5000 4 blaster cannons

--- code/ships.py
+++ code/ships.py
@@ -181,10 +181,12 @@ class Ship4(PlayableShip):
     def __init__(self, game):
         self.game = game
         self.path = "./ships/ship4.png"
-        self.hp = DeluxeHP(self.game, 2000000, 200, 700, 350, 30)
-        super().__init__(game, self.path, 0.98, 200, 200, 5000)
-        self.cannon = Blaster(self.game, self, Vector2(-23, -30), self.force, 0.4)
-        self.cannon2 = Blaster(self.game, self, Vector2(23, -30), self.force, 0.4)
+        self.hp = DeluxeHP(self.game, 3500000, 200, 700, 350, 30)
+        super().__init__(game, self.path, 0.98, 100, 500, 5000)
+        self.cannon = Blaster(self.game, self, Vector2(-25, -35), self.force, 0.4)
+        self.cannon2 = Blaster(self.game, self, Vector2(25, -35), self.force, 0.4)
+        self.cannon3 = Blaster(self.game, self, Vector2(-47, -30), self.force, 0.4)
+        self.cannon4 = Blaster(self.game, self, Vector2(47, -30), self.force, 0.4)
 
     def add_force(self, force):
         super().add_force(force)
@@ -193,6 +195,8 @@ class Ship4(PlayableShip):
         super().tick()
         self.cannon.tick()
         self.cannon2.tick()
+        self.cannon3.tick()
+        self.cannon4.tick()
     def draw(self):
         super().draw()
 
