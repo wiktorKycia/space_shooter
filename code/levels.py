c1bf399c14bc62d81638ded9f2bf7057b4e96bcf
Wiktor Kycia <wiktor.kycia@gmail.com>
2023-07-02 16:06:02 +0200

    4th stage: line 5 of Enemy2; delete stage 5

--- code/levels.py
+++ code/levels.py
@@ -324,9 +324,7 @@ class Level6(Level):
                     self.block.line(self.game.width / 2, 100, 3)
                     self.block.line(self.game.width / 2, 200, 3)
                 case 4:
-                    self.block.pair(self.game.width/2, 150, 2)
-                case 5:
-                    self.block.line(self.game.width / 2, 100, 3, 2)
+                    self.block.line(self.game.width / 2, 100, 3, 5)
                 case _:
                     self.game.showing = "gamemenu"
                     self.game.gamemenu.__init__(self.game)
