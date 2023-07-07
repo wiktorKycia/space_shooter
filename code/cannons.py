51c1ac9612eeae403118cb144cae851f0c63e17c
Wiktor Kycia <wiktor.kycia@gmail.com>
2023-07-07 16:21:27 +0200

    copy code from BaseCannon

--- code/cannons.py
+++ code/cannons.py
@@ -45,8 +45,47 @@ class BaseCannon:
             self.shot()
 
 class BaseShotGun:
-    def __init__(self, game):
+    def __init__(self, game, ship, translation: Vector2, force: int, interval: float, barrel_length,
+                 key=pygame.K_SPACE):
         self.game = game
+        self.ship = ship
+        self.pos = ship.pos
+        self.translation = translation
+        self.force = force
+        self.interval = interval
+        self.key = key
+
+        self.clock = 0
+        self.barrel = barrel_length
+
+    def shot(self):
+        pass
+
+    def calculate_kickback_force(self, bullet):
+        acc = -bullet.acc  # getting initial bullet velocity
+        try:  # time that bullet spent in the barrel
+            tim = Vector2(math.sqrt(2 * self.barrel / acc.x), math.sqrt(2 * self.barrel / acc.y))
+        except ZeroDivisionError:
+            if acc.x == 0:
+                tim = Vector2(0, math.sqrt(2 * self.barrel / acc.y))
+            elif acc.y == 0:
+                tim = Vector2(math.sqrt(2 * self.barrel / acc.x), 0)
+        vel1 = Vector2(acc.x * tim.x, acc.y * tim.y)  # getting vel of the bullet after escaping from barrel
+        vel = (bullet.mass * vel1) / self.ship.mass  # getting initial velocity from zasada zachowania pędu
+        vel.x *= vel.x
+        vel.y *= vel.y
+        energy = (self.ship.mass * vel) / 2  # calculating kinetic energy
+        force = energy / self.barrel  # calculating kickback force
+        return force
+
+    def tick(self):
+        self.clock += self.game.dt
+        self.pos = self.ship.pos + self.translation
+        # self.rect.center = self.pos
+        pressed = pygame.key.get_pressed()
+        if pressed[self.key] and self.clock > self.interval:
+            self.clock = 0
+            self.shot()
 
 class Kinetic60Gun(BaseCannon):
     def __init__(self, game, ship, translation:Vector2, force:int, interval:float, key=pygame.K_SPACE):
