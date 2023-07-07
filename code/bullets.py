62696f542a77a9fb5419428cc2e39be35d441d95
Wiktor Kycia <wiktor.kycia@gmail.com>
2023-07-07 18:36:20 +0200

    copy code from BaseBullet to ShotGunBullet

--- code/bullets.py
+++ code/bullets.py
@@ -41,8 +41,43 @@ class Bullet(object):
         self.game.screen.blit(self.hitbox, (self.pos.x - self.width/2, self.pos.y - self.height/2))
 
 class ShotGunBullet:
-    def __init__(self):
-        pass
+    def __init__(self, game, x, y, width, height, force, mass, color=(255, 255, 255), sound=None):
+        self.pos = Vector2(x, y)
+        self.vel = Vector2(0, 0)
+
+        self.mass = mass
+        acc = int(force / mass)
+        self.acc = Vector2(0, -acc)
+
+        self.width = width
+        self.height = height
+        # self.hitbox = pygame.Rect(self.pos.x - width / 2, self.pos.y - height / 2, width, height)
+        self.hitbox = pygame.Surface((self.width, self.height))
+        self.hitbox.fill(color)
+        # self.hitbox.topleft = (self.pos.x, self.pos.y)
+
+        self.mask = pygame.mask.from_surface(self.hitbox)
+
+        self.game = game
+        self.color = color
+
+        if sound is not None:
+            self.sound = mixer.Sound(sound)
+            self.sound.set_volume(0.1)
+
+    def tick(self):
+        # Physics
+        self.vel *= 0.9995
+
+        self.vel += self.acc
+        self.pos += self.vel * self.game.dt
+        self.acc *= 0
+
+    def draw(self):
+        # self.hitbox = pygame.Rect(self.pos.x - self.width / 2, self.pos.y - self.height / 2, self.width, self.height)
+        # pygame.draw.rect(self.game.screen, self.color, self.hitbox)
+        # self.hitbox.topleft = (self.pos.x, self.pos.y)
+        self.game.screen.blit(self.hitbox, (self.pos.x - self.width / 2, self.pos.y - self.height / 2))
 
 class Kinetic60Bullet(Bullet):
     def __init__(self, game, x, y, force):
