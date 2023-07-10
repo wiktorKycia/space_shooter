4c66767fdd9e626527ffe870036d21eae67c2dab
Wiktor Kycia <wiktor.kycia@gmail.com>
2023-07-10 09:50:58 +0200

    add a click_P_counter parameter to LevelsMenu

--- code/UI.py
+++ code/UI.py
@@ -201,6 +201,7 @@ class LevelsMenu:
         self.game = game
         self.button_back = Button(game, 50, 700, "./images/buttons/button_back.png", 1.0, "./images/buttons/button_back_hover.png")
         self.buttons = []
+        self.click_P_counter = 0
         for i, level in enumerate(self.game.levels):
             if (i+1) % 3 == 1:
                 self.buttons.append(LevelButton(self.game, self.game.width/5, self._calculate_level_y(i+1), 200, 100, i+1))
