82679d4bdf145de287030421d71659c277c4519a
Wiktor Kycia <wiktor.kycia@gmail.com>
2023-07-11 14:03:49 +0200

    add pos and clock parameters, copy tick method from BaseCannon

--- code/maneuvering_cannons.py
+++ code/maneuvering_cannons.py
@@ -7,7 +7,16 @@ class ManeuveringBulletsLauncher:
     def __init__(self, game, ship, translation: Vector2, force: int, interval: float, key=pygame.K_SPACE):
         self.game = game
         self.ship = ship
+        self.pos = ship.pos
         self.translation = translation
         self.force = force
         self.interval = interval
         self.key = key
+        self.clock = 0
+
+    def tick(self):
+        self.clock += self.game.dt
+        self.pos = self.ship.pos + self.translation
+        pressed = pygame.key.get_pressed()
+        if pressed[self.key] and self.clock > self.interval:
+            self.clock = 0
