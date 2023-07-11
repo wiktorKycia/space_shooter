47934effb825db50992d843ea6b508a152ed8f1c
Wiktor Kycia <wiktor.kycia@gmail.com>
2023-07-11 13:06:07 +0200

    import mixer

--- code/bullets2.py
+++ code/bullets2.py
@@ -1,5 +1,7 @@
 import pygame
+from pygame import mixer
 from pygame.math import *
+mixer.init()
 
 class ManeuveringBullet:
     def __init__(self, game, x, y, width, height, force, mass, color=(255, 255, 255), sound=None):
