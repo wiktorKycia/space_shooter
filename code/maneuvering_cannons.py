666a837aae5d54e8d598e5ec69714ee425af8619
Wiktor Kycia <wiktor.kycia@gmail.com>
2023-07-11 13:59:03 +0200

    create ManeuveringBulletsLauncher class, copy init arguments from BaseCannon

--- code/maneuvering_cannons.py
+++ code/maneuvering_cannons.py
@@ -2,3 +2,6 @@ import pygame
 from pygame import mixer
 from pygame.math import *
 mixer.init()
+
+class ManeuveringBulletsLauncher:
+    def __init__(self, game, ship, translation: Vector2, force: int, interval: float, barrel_length, key=pygame.K_SPACE):
