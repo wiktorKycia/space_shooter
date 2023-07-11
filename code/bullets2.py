2ffef1b6cd7e2aa8d6111fd69f2fdbc2f4b4f37e
Wiktor Kycia <wiktor.kycia@gmail.com>
2023-07-11 13:06:40 +0200

    create fields from the arguments, just like in BaseBullet

--- code/bullets2.py
+++ code/bullets2.py
@@ -5,3 +5,18 @@ mixer.init()
 
 class ManeuveringBullet:
     def __init__(self, game, x, y, width, height, force, mass, color=(255, 255, 255), sound=None):
+        self.game = game
+
+        self.x = x
+        self.y = y
+
+        self.width = width
+        self.height = height
+
+        self.force = force
+        self.mass = mass
+
+        self.color = color
+        if sound is not None:
+            self.sound = mixer.Sound(sound)
+            self.sound.set_volume(0.1)
