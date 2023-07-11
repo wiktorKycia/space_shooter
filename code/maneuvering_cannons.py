47ef372499458d3e6943470f741c3d1169a4fd78
Wiktor Kycia <wiktor.kycia@gmail.com>
2023-07-11 14:00:55 +0200

    create fields from these arguments, delete barrel_length parametes

--- code/maneuvering_cannons.py
+++ code/maneuvering_cannons.py
@@ -4,4 +4,10 @@ from pygame.math import *
 mixer.init()
 
 class ManeuveringBulletsLauncher:
-    def __init__(self, game, ship, translation: Vector2, force: int, interval: float, barrel_length, key=pygame.K_SPACE):
+    def __init__(self, game, ship, translation: Vector2, force: int, interval: float, key=pygame.K_SPACE):
+        self.game = game
+        self.ship = ship
+        self.translation = translation
+        self.force = force
+        self.interval = interval
+        self.key = key
