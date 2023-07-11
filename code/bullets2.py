8d96d642b517c7fb2a7910bc679679feeec1a526
Wiktor Kycia <wiktor.kycia@gmail.com>
2023-07-11 17:51:11 +0200

    in tick in ManeuveringBullet move moving section before first if statement

--- code/bullets2.py
+++ code/bullets2.py
@@ -41,6 +41,10 @@ class ManeuveringBullet:
             self.maneuvering = True
 
     def tick(self):
+        self.vel *= 0.9995
+        self.vel += self.acc
+        self.pos += self.vel * self.game.dt
+        self.acc *= 0
         if self.maneuvering:
             if self.enemy in self.game.enemies:
                 vector = Vector2(self.enemy.pos.x - self.pos.x, self.enemy.pos.y - self.pos.y)
@@ -48,20 +52,11 @@ class ManeuveringBullet:
                 angle = self.vel.angle_to(vector) / 100
                 print(angle, end=" ")
                 self.vel.rotate_ip(angle)
-                self.pos += self.vel * self.game.dt
-                self.pos.rotate(angle)
                 print(self.vel.angle_to(vector))
                 # print(dir(self.vel))
                 # self.hitbox= pygame.transform.rotate(self.hitbox,angle)
-                self.vel *= 0.9995
-                self.vel += self.acc
-                self.acc *= 0
+
             else: self.maneuvering = False
-        else:
-            self.vel *= 0.9995
-            self.vel += self.acc
-            self.pos += self.vel * self.game.dt
-            self.acc *= 0
 
     def draw(self):
         self.game.screen.blit(self.hitbox, (self.pos.x - self.width/2, self.pos.y - self.height/2))
