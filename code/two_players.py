e42e433afe8cc70ffbe87d0d722b6472e64f88ec
Wiktor Kycia <wiktor.kycia@gmail.com>
2023-07-14 10:16:34 +0200

    in both of the Player1 and Player2, inherit init method

--- code/two_players.py
+++ code/two_players.py
@@ -3,10 +3,12 @@ from code.ships import PlayableShip
 
 
 class Player1(PlayableShip):
-    pass
+    def __init__(self, game):
+        super().__init__(game, "./images/SpaceShips2/spaceship_small_blue.png", 0.98, 200, 100, 2000)
 
 class Player2(PlayableShip):
-    pass
+    def __init__(self, game):
+        super().__init__(game, "./images/SpaceShips2/spaceship_small_red.png", 0.98, 200, 100, 2000)
 
 class TwoPlayersGame:
     def __init__(self, game):
