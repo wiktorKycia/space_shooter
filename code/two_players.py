996f397401def008715bc1b27067042a3bc59c5a
Wiktor Kycia <wiktor.kycia@gmail.com>
2023-07-14 10:27:45 +0200

    add tick method to Player2

--- code/two_players.py
+++ code/two_players.py
@@ -22,6 +22,9 @@ class Player2(PlayableShip):
     def __init__(self, game):
         super().__init__(game, "./images/SpaceShips2/spaceship_small_red.png", 0.98, 200, 100, 2000)
 
+    def tick(self):
+
+
 class TwoPlayersGame:
     def __init__(self, game):
         pass
