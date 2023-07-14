aaf43069ecf3f570d2ee866186f02ef116059e85
Wiktor Kycia <wiktor.kycia@gmail.com>
2023-07-14 10:11:13 +0200

    import PlayableShip from code.ships, inherit Player1 and Player2 classes from PlayableShip

--- code/two_players.py
+++ code/two_players.py
@@ -1,10 +1,13 @@
 import pygame
+from code.ships import PlayableShip
 
-class Player1:
+
+class Player1(PlayableShip):
     pass
 
-class Player2:
+class Player2(PlayableShip):
     pass
 
 class TwoPlayersGame:
     def __init__(self, game):
+        pass
