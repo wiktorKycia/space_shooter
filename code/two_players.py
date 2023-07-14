67d972f13ba3d457286d7eb0fad78a44921e296d
Wiktor Kycia <wiktor.kycia@gmail.com>
2023-07-14 10:20:05 +0200

    inherit tick method from PlayableShip1 to Player1

--- code/two_players.py
+++ code/two_players.py
@@ -6,6 +6,10 @@ class Player1(PlayableShip):
     def __init__(self, game):
         super().__init__(game, "./images/SpaceShips2/spaceship_small_blue.png", 0.98, 200, 100, 2000)
 
+    def tick(self):
+        super().tick()
+
+
 class Player2(PlayableShip):
     def __init__(self, game):
         super().__init__(game, "./images/SpaceShips2/spaceship_small_red.png", 0.98, 200, 100, 2000)
