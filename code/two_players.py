ec3978585d2389695ed9e076436214e5545d8749
Wiktor Kycia <wiktor.kycia@gmail.com>
2023-07-14 10:34:46 +0200

    add KineticGun to Player2, values the same like in Player1, but key=right_shift

--- code/two_players.py
+++ code/two_players.py
@@ -22,6 +22,7 @@ class Player2(PlayableShip):
     def __init__(self, game):
         self.hp = DeluxeHP(self.game, 1000000, 120, 30, 200, 20)
         super().__init__(game, "./images/SpaceShips2/spaceship_small_red.png", 0.98, 200, 100, 2000)
+        self.cannon = KineticGun(game, self, Vector2(0, 20), self.force, 0.5, pygame.K_RSHIFT)
 
     def tick(self):
         # Input
