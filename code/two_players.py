ec7ea5f00436c07a8f6bdb1f037ba5730be4f1f1
Wiktor Kycia <wiktor.kycia@gmail.com>
2023-07-14 10:30:12 +0200

    Revert "copy tick method from PlayableShip to Player2"
    This reverts commit dec4d68e949d7e3bc470d8f5ee932ab36261ed11.

--- code/two_players.py
+++ code/two_players.py
@@ -23,45 +23,7 @@ class Player2(PlayableShip):
         super().__init__(game, "./images/SpaceShips2/spaceship_small_red.png", 0.98, 200, 100, 2000)
 
     def tick(self):
-        # Input
-        pressed = pygame.key.get_pressed()
-        if pressed[pygame.K_UP]:
-            self.add_force(Vector2(0, -self.force))
-        if pressed[pygame.K_DOWN]:
-            self.add_force(Vector2(0, self.force))
-        if pressed[pygame.K_RIGHT]:
-            self.add_force(Vector2(self.force, 0))
-        if pressed[pygame.K_LEFT]:
-            self.add_force(Vector2(-self.force, 0))
 
-        # Physics
-        self.vel *= self.slip
-        self.vel -= Vector2(0, 0)
-
-        self.vel += self.acc
-
-        # Limiting speed
-        if self.vel.x > self.max_speed:  # right
-            self.vel = Vector2(self.max_speed, self.vel.y)
-        elif self.vel.x < -self.max_speed:  # left
-            self.vel = Vector2(-self.max_speed, self.vel.y)
-        if self.vel.y > self.max_speed:  # up
-            self.vel = Vector2(self.vel.x, self.max_speed)
-        elif self.vel.y < -self.max_speed:  # down
-            self.vel = Vector2(self.vel.x, -self.max_speed)
-
-        self.pos += self.vel * self.game.dt
-        self.acc *= 0
-
-        self.hitbox.center = (self.pos.x, self.pos.y)
-
-        self.clock += self.game.dt
-
-        for bullet in self.bullets:
-            if bullet.pos.y <= -bullet.height:
-                self.bullets.remove(bullet)
-            else:
-                bullet.tick()
 
 class TwoPlayersGame:
     def __init__(self, game):
