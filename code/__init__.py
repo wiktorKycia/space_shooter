d32614c1bd574d085ded61d020052f136f55c120
Wiktor Kycia <wiktor.kycia@gmail.com>
2023-07-23 12:25:27 +0200

    add write method, that creates a surface from the given text, font_size, color, font_style and centers if needed; to text attribute assign the result of the write method, pass the arguments from init parameters

--- code/__init__.py
+++ code/__init__.py
@@ -38,6 +38,14 @@ class UnClickable(StaticObject):
         self.game.screen.blit(self.surf, (self.x, self.y))
 
 class TextObject(UnClickable):
-    def __init__(self, game, x, y, ):
-        self.text =
-        super().__init__(game, x, y, )
+    def __init__(self, game, x, y, text, font_size, color=(255, 255, 255), font_style="Arial", is_centered=False):
+        self.text = self.write(text, font_size, color, font_style, is_centered)
+        super().__init__(game, x, y, self.text)
+
+    def write(self, text, font_size, color=(255, 255, 255), font_style="Arial", is_centered=False):
+        font = pygame.font.SysFont(font_style, font_size)
+        rend = font.render(text, True, color)
+        if is_centered is True:
+            self.x = (self.game.width - rend.get_rect().width) / 2
+            self.y = (self.game.height - rend.get_rect().height) / 2
+        return rend
