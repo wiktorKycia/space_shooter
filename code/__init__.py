f1ba3e76772ba06da55cd84bb0e022652d6f0d63
Wiktor Kycia <wiktor.kycia@gmail.com>
2023-07-23 12:46:02 +0200

    add init method, parameters: game, x, y, path, scale=1.0, path2=''; call super init method, load image from path, scale this image, if path2 is not empty, then do the same with the second image; get the width and height of the images and the rect

--- code/__init__.py
+++ code/__init__.py
@@ -60,3 +60,16 @@ class ImageObject(UnClickable):
 
 
 class Clickable(StaticObject):
+    def __init__(self, game, x, y, path, scale=1.0, path2=""):
+        super().__init__(game, x, y)
+        self.image = pygame.image.load(path)
+        if scale != 1.0: pygame.transform.scale_by(self.image, scale)
+        if path2 != "":
+            self.image2 = pygame.image.load(path2)
+            if scale != 1.0: pygame.transform.scale_by(self.image2, scale)
+
+        self.width = self.image.get_width()
+        self.height = self.image.get_height()
+
+        self.rect = self.image.get_rect()
+        self.rect.center = (x, y)
