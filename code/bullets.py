dfdf1f94fb0d9a7de04d2b8a7cdf43552550c692
Wiktor Kycia <wiktor.kycia@gmail.com>
2023-08-11 19:01:02 +0200

    testing new bullet images from internet

--- code/bullets.py
+++ code/bullets.py
@@ -5,7 +5,7 @@ from code import NoShooting
 mixer.init()
 
 class ImageBullet(NoShooting):
-    def __init__(self, game, x, y, path, mass, force,scale=1.0):
+    def __init__(self, game, x, y, path, mass, force, scale=1.0):
         super().__init__(game, x, y, path, mass, scale)
         self.add_force(-force)
 
