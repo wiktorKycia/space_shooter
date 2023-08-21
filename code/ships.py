8e29888bc700f10f2760eb0da8a6cec5797179fc
Wiktor Kycia <wiktor.kycia@gmail.com>
2023-08-21 15:16:12 +0200

    change Ship2's second gun y to 665

--- code/ships.py
+++ code/ships.py
@@ -85,7 +85,7 @@ class Ship2(PlayableShip):
             hp_x=165, hp_y=710
         )
         self.gun = KineticGun(game, self, Vector2(-27, -20), self.force, key=pygame.K_KP_1)
-        self.gun2 = KineticGun(game, self, Vector2(27, -20), self.force, key=pygame.K_KP_2, bar_y=670)
+        self.gun2 = KineticGun(game, self, Vector2(27, -20), self.force, key=pygame.K_KP_2, bar_y=665)
         # self.cannon = Blaster(self.game, self, Vector2(27, -20), self.force, 0.35)
         # self.cannon2 = Blaster(self.game, self, Vector2(-27, -20), self.force, 0.35)
 
