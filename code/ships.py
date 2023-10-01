9f29a506c605a36d47fbe3093ea03e5dea8f2055
Wiktor Kycia <wiktor.kycia@gmail.com>
2023-10-01 14:24:15 +0200

    do the same with Ship3, Ship4 and Ship5

--- code/ships.py
+++ code/ships.py
@@ -94,3 +94,57 @@ class Ship2(PlayableShip):
                 LaserLight(game, self, Vector2(0, -20), self.force, key=pygame.K_KP_1)
             ]
         )
+
+class Ship3(PlayableShip):
+    def __init__(self, game):
+        super().__init__(
+            game, "./images/SpaceShips/Ship_3.png",
+            mass=300,
+            max_speed=275,
+            force=1500,
+            hp_amount=4000000,
+            hp_height=25, hp_width=300,
+            hp_x=165, hp_y=710,
+            scale=2.0
+        )
+        self.guns.extend(
+            [
+                LaserLight(game, self, Vector2(0, -20), self.force, key=pygame.K_KP_1)
+            ]
+        )
+
+class Ship4(PlayableShip):
+    def __init__(self, game):
+        super().__init__(
+            game, "./images/SpaceShips/Ship_4.png",
+            mass=300,
+            max_speed=275,
+            force=1500,
+            hp_amount=4000000,
+            hp_height=25, hp_width=300,
+            hp_x=165, hp_y=710,
+            scale=2.0
+        )
+        self.guns.extend(
+            [
+                LaserLight(game, self, Vector2(0, -20), self.force, key=pygame.K_KP_1)
+            ]
+        )
+
+class Ship5(PlayableShip):
+    def __init__(self, game):
+        super().__init__(
+            game, "./images/SpaceShips/Ship_5.png",
+            mass=300,
+            max_speed=275,
+            force=1500,
+            hp_amount=4000000,
+            hp_height=25, hp_width=300,
+            hp_x=165, hp_y=710,
+            scale=2.0
+        )
+        self.guns.extend(
+            [
+                LaserLight(game, self, Vector2(0, -20), self.force, key=pygame.K_KP_1)
+            ]
+        )
