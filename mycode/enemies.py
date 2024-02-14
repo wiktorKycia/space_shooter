ac3de9011d6bcb08e7b1d4586616928b99801614
Wiktor Kycia <wiktor.kycia@gmail.com>
2024-02-14 18:38:14 +0100

    do the same in BaseEnemy class in tick method

--- mycode/enemies.py
+++ mycode/enemies.py
@@ -19,8 +19,8 @@ class BaseEnemy(ShootingDownNoMove):
             gun.tick()
             for bullet in gun.bullets:
                 if bullet.check_collision(self.game.player.current_ship):
-                    energy = int((bullet.mass * bullet.vel * bullet.vel) / 2)
-                    self.game.player.current_ship.hp.get_damage(energy)
+                    # energy = int((bullet.mass * bullet.vel * bullet.vel) / 2)
+                    self.game.player.current_ship.hp.get_damage(bullet.damage)
                     gun.bullets.remove(bullet)
                     del bullet
 
