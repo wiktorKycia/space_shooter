9f494a5988cfdcce9c8b2d58d8720c5f925d21fa
Wiktor Kycia <wiktor.kycia@gmail.com>
2024-02-16 19:03:29 +0100

    comment BaseEnemy class, as later, we won't need it anymore

--- mycode/enemies.py
+++ mycode/enemies.py
@@ -40,28 +40,28 @@ class MovingEnemy(ShootingDown):
                 bullet.draw()
 
 
-class BaseEnemy(ShootingDownNoMove):
-    def __init__(self, game, x, y, path, force, hp_amount, hp_width=50, hp_height=10):
-        super().__init__(game, x, y, path, force, hp_amount, hp_width, hp_height)
-        self.guns = []
-        self.is_shooting = True
-
-    def tick(self):
-        super().tick()
-        for gun in self.guns:
-            gun.tick()
-            for bullet in gun.bullets:
-                if bullet.check_collision(self.game.player.current_ship):
-                    # energy = int((bullet.mass * bullet.vel * bullet.vel) / 2)
-                    self.game.player.current_ship.hp.get_damage(bullet.damage)
-                    gun.bullets.remove(bullet)
-                    del bullet
-
-    def draw(self):
-        super().draw()
-        for gun in self.guns:
-            for bullet in gun.bullets:
-                bullet.draw()
+# class BaseEnemy(ShootingDownNoMove):
+#     def __init__(self, game, x, y, path, force, hp_amount, hp_width=50, hp_height=10):
+#         super().__init__(game, x, y, path, force, hp_amount, hp_width, hp_height)
+#         self.guns = []
+#         self.is_shooting = True
+#
+#     def tick(self):
+#         super().tick()
+#         for gun in self.guns:
+#             gun.tick()
+#             for bullet in gun.bullets:
+#                 if bullet.check_collision(self.game.player.current_ship):
+#                     # energy = int((bullet.mass * bullet.vel * bullet.vel) / 2)
+#                     self.game.player.current_ship.hp.get_damage(bullet.damage)
+#                     gun.bullets.remove(bullet)
+#                     del bullet
+#
+#     def draw(self):
+#         super().draw()
+#         for gun in self.guns:
+#             for bullet in gun.bullets:
+#                 bullet.draw()
 
 class Enemy1(BaseEnemy):
     def __init__(self, game, x, y):
