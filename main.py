35a47e72a4eb7336fcab901fc9af377fac9fc5f3
Wiktor Kycia <wiktor.kycia@gmail.com>
2024-07-26 17:26:12 +0200

    create Bouncer2 as an enemy that changes the place every 5s, add level 12 and introduce it into a game

--- main.py
+++ main.py
@@ -39,7 +39,7 @@ class Game(object):
             Level1, Level2, Level3,
             Level4, Level5, Level6,
             Level7, Level8, Level9,
-            Level10, Level11
+            Level10, Level11, Level12
         ]
         self.menuHandler = MenuHandler(self, MainMenu)
 
