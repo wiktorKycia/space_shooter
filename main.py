1895711c42d504304c337d01d85e27a47f17a6fb
Wiktor Kycia <wiktor.kycia@gmail.com>
2025-01-06 16:06:04 +0100

    apply Dependency Inversion to player, mouse and manuHandler objects; also add comments to the code in main while loop

--- main.py
+++ main.py
@@ -18,6 +18,9 @@ class Game(object):
     """
 
     def __init__(self,
+        player: Player,
+        mouse: Mouse,
+        menuHandler: MenuHandler,
                  levelManager: LevelManager,
                  screen: pygame.Surface,
                  max_tps: float = 100.0,
@@ -36,24 +39,34 @@ class Game(object):
         self.isrun = True
 
         #loading objects
-        self.player = Player(self)
-        self.mouse = Mouse(self)
+        self.player = player
+        self.mouse = mouse
 
         self.levelManager = levelManager
-
-        self.menuHandler = MenuHandler(self, MainMenu)
-
+        
+        self.menuHandler = menuHandler
+        
         while self.isrun:
+            # Events
             for event in pygame.event.get():
                 if event.type == pygame.QUIT:
                     self.isrun = False
+            
+            # Update time
             self.dt = self.tps_clock.get_time() / 1000
             self.tps_clock.tick(self.tps_max)
+            
+            # Draw the background
             self.screen.fill((0, 0, 0))
-
+            
+            # Move and draw objects
             self.tick()
             self.draw()
+            
+            # Do the next tick
             pygame.display.update()
+        
+        # Exit
         pygame.quit()
         quit()
 
@@ -66,9 +79,14 @@ class Game(object):
 if __name__ == "__main__":
     width, height = (750, 750)
     screen = pygame.display.set_mode((width, height))
+    
+    player = Player()
+    mouse = Mouse()
+    
+    menuHandler = MenuHandler(self, MainMenu)
 
     enemySpawner: EnemySpawner = EnemySpawner(self)
     waveManager: WaveManager = WaveManager(self, "./gameData/levels.json", enemySpawner)
     levelManager: LevelManager = LevelManager(waveManager)
-
-    game: Game = Game(levelManager, screen, 100.0)
+    
+    game: Game = Game(player, mouse, menuHandler, levelManager, screen, 100.0)
