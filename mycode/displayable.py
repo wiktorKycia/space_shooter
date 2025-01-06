92795e7efaae6f7aa1aa2e2c7f435c58e7aa14f6
Wiktor Kycia <wiktor.kycia@gmail.com>
2025-01-06 18:38:46 +0100

    create file for objects that have Image, hitbox and mask, create classes: Displayable and DisplayableObjectBuilder

---
+++ mycode/displayable.py
@@ -0,0 +1,30 @@
+import pygame
+
+
+class Displayable:
+    def __init__(self, image: pygame.Surface, scale: float = 1.0):
+        self.image = image if not scale != 1.0 else pygame.transform.scale_by(image, scale)
+        
+        self.width = self.image.get_width()
+        self.height = self.image.get_height()
+        
+        self.hitbox = self.image.get_rect()
+        self.mask = pygame.mask.from_surface(self.image)
+    
+    def tick(self, x: float, y: float):
+        self.hitbox.center = (x, y)
+    
+    def draw(self, screen: pygame.Surface, x: float, y: float):
+        """
+        blits the image in the current position
+        :return:
+        """
+        screen.blit(self.image, (x - self.width / 2, y - self.height / 2))
+
+
+class DisplayableObjectBuilder:
+    def __init__(self, path: str):
+        self.__image: pygame.Surface = pygame.image.load(path).convert_alpha()
+    
+    def create(self):
+        return self.__image
