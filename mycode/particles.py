d2b27345cc199a7801e78c52dca23198ba276bba
Wiktor Kycia <wiktor.kycia@gmail.com>
2025-01-10 18:59:21 +0100

    adjust Particle class to the new style of code

--- mycode/particles.py
+++ mycode/particles.py
@@ -1,6 +1,7 @@
 from mycode.projectile import Projectile
 from mycode.physics import PygamePhysics
 from mycode.spacecraft import Spacecraft
+from mycode.displayable import Displayer
 import pygame
 
 class Particle(Projectile):
@@ -10,33 +11,34 @@ class Particle(Projectile):
         
         self.surf = pygame.Surface((self.radius * 2, self.radius * 2), pygame.SRCALPHA)
         
+        self.displayer = Displayer(self.surf)
+        
         self.alpha = 100
         self.green = 0
         
         self.current_damage = damage
     
     def check_collision(self, ship: Spacecraft):
-        return self.hitbox.colliderect(ship.displayer.hitbox)
+        return self.displayer.hitbox.colliderect(ship.displayer.hitbox)
     
-    def tick(self):  # TODO: naprawić: zrobić tak, żeby zadawało damage
-        if self.clock > 0.05:
-            self.clock -= 0.05
+    def tick(self, dt):
+        if self.physics.clock > 0.05:
+            self.physics.clock -= 0.05
             if self.alpha > 5:
-                self.alpha -= 1 * self.game.dt
+                self.alpha -= 1 * dt
             if self.green < 230:
-                self.green += 200 * self.game.dt
+                self.green += 200 * dt
                 if self.green > 230:
                     self.green = 230
-            self.radius += 30 * self.game.dt
+            self.radius += 30 * dt
             self.surf = pygame.Surface((self.radius * 2, self.radius * 2), pygame.SRCALPHA)
-            self.hitbox = self.surf.get_rect()
+            self.displayer.hitbox = self.surf.get_rect()
             self.current_damage = self.alpha / 100 * self.damage
-        super().tick()
-        # if self.alpha < 10 or self.pos.y < 0:
-        #     print("self delete")
-        #     del self
+        
+        self.physics.tick(dt)
+        self.displayer.tick(self.physics.pos.x, self.physics.pos.y)
     
-    def draw(self):
+    def draw(self, screen: pygame.Surface):
         color = (255, self.green, 0, self.alpha)
         pygame.draw.circle(self.surf, color, (self.surf.get_width() // 2, self.surf.get_height() // 2), self.radius)
-        self.game.screen.blit(self.surf, self.surf.get_rect(center=(self.pos.x, self.pos.y)))
+        screen.blit(self.surf, self.surf.get_rect(center=(self.physics.pos.x, self.physics.pos.y)))
