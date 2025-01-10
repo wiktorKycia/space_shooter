7d901cac7459735054d07ee40413d906898fcb33
Wiktor Kycia <wiktor.kycia@gmail.com>
2025-01-10 19:20:37 +0100

    refactor LaserBeam class

--- mycode/laserBeams.py
+++ mycode/laserBeams.py
@@ -1,40 +1,30 @@
-from mycode.projectile import Projectile
+from mycode.spacecraft import Spacecraft
+import pygame
 
 
-class LaserBeam(Projectile):
-    def __init__(self, game, laser, x, y, damage=10):
-        self.laser = laser
-        super().__init__(game, x, y, pygame.Surface((0, 0)), 0)
-        self.line = ((0, 0), (0, 0))
-        self.base_damage = damage
-        self.damage = self.base_damage
+class LaserBeam:
+    def __init__(self, damage: int = 10):
+        self.line: tuple[tuple[float, float], tuple[float, float]] | None = None
+        self.damage = damage
     
-    def check_collision(self, ship):
-        return ship.hitbox.clipline(self.line)
+    def check_collision(self, ship: Spacecraft):
+        return ship.displayer.hitbox.clipline(self.line)
     
-    def tick(self):
-        coords = self.laser.slot.ship.getClosestEnemy()
-        if coords is not None and self.laser.active:
-            self.line = (
-                (self.laser.slot.pos.x + self.laser.slot.translation.x,
-                 self.laser.slot.pos.y + self.laser.slot.translation.y),
-                coords
-                # if coords is not None else (
-                #     self.laser.slot.pos.x + self.laser.slot.translation.x,
-                #     self.laser.slot.pos.y + self.laser.slot.translation.y)
-            )
-            self.damage = self.base_damage * self.game.dt
-            if self.laser.is_player:
-                for enemy in self.game.menuHandler.currentMenu.enemies:
-                    if self.check_collision(enemy):
-                        enemy.hp.get_damage(self.damage)
-            else:
-                if self.check_collision(self.game.player.current_ship):
-                    self.game.player.current_ship.hp.get_damage(self.damage)
+    def set_aim(self, ship_coords: tuple[float, float], enemy_coords: tuple[float, float], trigger: bool):
+        if trigger:
+            self.line = (ship_coords, enemy_coords)
+    
+    def deal_damage(self, damage, enemies: list[Spacecraft]):
+        for enemy in enemies:
+            if self.check_collision(enemy):
+                enemy.hp.damage(damage)
+    
+    def tick(self, dt, enemies: list[Spacecraft]):
+        if self.line is not None:
+            self.deal_damage(self.damage * dt, enemies)
         else:
             self.line = None
-        del coords
     
-    def draw(self):
+    def draw(self, screen: pygame.Surface):
         if self.line is not None:
-            pygame.draw.line(self.game.screen, (255, 255, 255), self.line[0], self.line[1], 2)
+            pygame.draw.line(screen, (255, 255, 255), self.line[0], self.line[1], 2)
