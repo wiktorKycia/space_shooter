a39c51dada9733773ab5a2a4058e969220ec5375
Wiktor Kycia <wiktor.kycia@gmail.com>
2025-01-13 13:11:30 +0100

    remove 'game' parameter from all the classes in mycode.__init__.py

--- mycode/__init__.py
+++ mycode/__init__.py
@@ -4,43 +4,23 @@ from mycode.other import *
 from mycode.general import *
 import math
 
-class MainObject(object):
-    """
-    This is the main class,
-    it basically is an abstract class for every object in the game.
-    """
-    def __init__(self):
-        pass
 
-    def tick(self):
-        """
-        Method tick contains instructions to run during every tick (frame).
-        """
-        pass
-
-    def draw(self):
-        """
-        Method draw usually is called after tick method, it displays object on the screen.
-        """
-        pass
-
-class StaticObject(MainObject):
+class StaticObject:
     """
     Static Object is the parent class for every object in the game, that does not move.
     It has general use in user interface.
     It has only the x, y as coordinates and game parameters.
     """
-    def __init__(self, game, x, y):
+    
+    def __init__(self, x, y):
         """
         Static Object is the parent class for every object in the game, that does not move.
         It has general use in user interface.
         It has only the x, y as coordinates and game parameters.
-        :param game: pass here the instance of Game class from main.py
         :param x: x coordinate
         :param y: y coordinate
         """
         super().__init__()
-        self.game = game
         self.x = x
         self.y = y
 
@@ -51,33 +31,36 @@ class UnClickable(StaticObject):
     (They can be clicked, but there will be no reaction from the program).
     it takes the same parameters as parent class (StaticObject), plus one more: surf.
     """
-    def __init__(self, game, x, y, surf):
+    
+    def __init__(self, x, y, surf):
         """
         The class that is a parent of static objects, that cannot be clicked.
         (They can be clicked, but there will be no reaction from the program).
         it takes the same parameters as parent class (StaticObject), plus one more: surf.
-        :param game: pass here the instance of Game class from main.py
         :param x: x coordinate
         :param y: y coordinate
         :param surf: the surface, that will be displayed on the screen
         """
-        super().__init__(game, x, y)
+        super().__init__(x, y)
         self.surf = surf
-
-    def draw(self):
+    
+    def draw(self, screen: pygame.Surface):
         """
         Blits the (given in init) surface on the screen
         """
-        self.game.screen.blit(self.surf, (self.x, self.y))
+        screen.blit(self.surf, (self.x, self.y))
 
 class TextObject(UnClickable):
     """
     The class certainly for displaying text on the screen
     """
-    def __init__(self, game, x, y, text, font_size, color=(255, 255, 255), font_style="Arial", is_centered=False):
+    
+    def __init__(
+        self, screen: pygame.Surface, x, y, text, font_size, color=(255, 255, 255), font_style="Arial",
+        is_centered=False
+    ):
         """
         initializes the text and calls 'write' method
-        :param game: pass here the instance of Game class from main.py
         :param x: x coordinate
         :param y: y coordinate
         :param text: the text that will be displayed on the screen, can have a fstring
@@ -86,10 +69,12 @@ class TextObject(UnClickable):
         :param font_style: the font of the given text, default: Arial
         :param is_centered: a boolean value, that determines if the text will be placed in the center or not, when True value is given, the x and y coordinates will be omitted and the text will be centered
         """
-        self.text = self.write(text, font_size, color, font_style, is_centered)
-        super().__init__(game, x, y, self.text)
-
-    def write(self, text:str, font_size, color=(255, 255, 255), font_style="Arial", is_centered=False):
+        self.text = self.write(screen, text, font_size, color, font_style, is_centered)
+        super().__init__(x, y, self.text)
+    
+    def write(
+        self, screen: pygame.Surface, text: str, font_size, color=(255, 255, 255), font_style="Arial", is_centered=False
+    ):
         """
         renders the text, returns pygame.Surface type
         :param text: the text displayed
@@ -102,22 +87,21 @@ class TextObject(UnClickable):
         font = pygame.font.SysFont(font_style, font_size)
         rend = font.render(text, True, color)
         if is_centered is True:
-            self.x = (self.game.width - rend.get_rect().width) / 2
-            self.y = (self.game.height - rend.get_rect().height) / 2
+            self.x = (screen.get_width() - rend.get_rect().width) / 2
+            self.y = (screen.get_height() - rend.get_rect().height) / 2
         return rend
-
-    def draw(self):
+    
+    def draw(self, screen: pygame.Surface):
         """
         blits the text on the screen
         :return:
         """
-        self.game.screen.blit(self.text, (self.x - self.text.get_width()/2, self.y - self.text.get_height()/2))
+        screen.blit(self.text, (self.x - self.text.get_width() / 2, self.y - self.text.get_height() / 2))
 
 class ImageObject(UnClickable):
-    def __init__(self, game, x:float, y:float, path:str, scale=1.0):
+    def __init__(self, x: float, y: float, path: str, scale=1.0):
         """
         Represent an image in game with specified path to an image and scale, that can be omitted
-        :param game:
         :param x: x coordinate
         :param y: y coordinate
         :param path: string, path to the image
@@ -126,29 +110,28 @@ class ImageObject(UnClickable):
         self.image = pygame.image.load(path).convert_alpha()
         if scale != 1.0:
             self.image = pygame.transform.scale_by(self.image, scale)
-        super().__init__(game, x, y, self.image)
-
-    def draw(self):
+        super().__init__(x, y, self.image)
+    
+    def draw(self, screen: pygame.Surface):
         """
         Blits the image on the screen
         :return:
         """
-        self.game.screen.blit(self.image, (self.x - self.image.get_width() / 2, self.y - self.image.get_height() / 2))
+        screen.blit(self.image, (self.x - self.image.get_width() / 2, self.y - self.image.get_height() / 2))
 
 
 class Clickable(StaticObject):
-    def __init__(self, game, x, y, path, scale=1.0, path2=""):
+    def __init__(self, x, y, path, scale=1.0, path2=""):
         """
         A class for the object, that can be clicked,
         it might have 2 images, as one will be shown only when mouse is hovering over the image rectangle
-        :param game: game object
         :param x: x coordinate
         :param y: y coordinate
         :param path: path to the main image
         :param scale: scale of the image
         :param path2: path to image shown, when mouse is over the object
         """
-        super().__init__(game, x, y)
+        super().__init__(x, y)
         self.image = pygame.image.load(path).convert_alpha()
         if scale != 1.0: self.image = pygame.transform.scale_by(self.image, scale)
         if path2 != "":
@@ -163,8 +146,8 @@ class Clickable(StaticObject):
 
         # current image
         self.img = self.image
-
-    def check_click(self):
+    
+    def check_click(self, mouse: Mouse):
         """
         sets action variable to False\n
         checks the mouse position,\n
@@ -180,18 +163,18 @@ class Clickable(StaticObject):
         if self.rect.collidepoint(pos):
             self.img = self.image2
             # check if the mouse is clicked
-            if self.game.mouse.click():
+            if mouse.click():
                 action = True
         elif not self.rect.collidepoint(pos):
             self.img = self.image
 
         return action
-
-    def draw(self):
-        self.game.screen.blit(self.img, (self.x - self.width/2, self.y - self.height/2))
+    
+    def draw(self, screen: pygame.Surface):
+        screen.blit(self.img, (self.x - self.width / 2, self.y - self.height / 2))
 
 class TextButton(StaticObject):
-    def __init__(self, game, x, y, width, height, text):
+    def __init__(self, x, y, width, height, text):
         """
         Represents a button, that has text in it
         by default it has background color of rgb(30, 30, 30),
@@ -203,7 +186,7 @@ class TextButton(StaticObject):
         :param height: height of the button
         :param text: text displayed in the button
         """
-        super().__init__(game, x, y)
+        super().__init__(x, y)
         self.width = width
         self.height = height
 
@@ -214,8 +197,8 @@ class TextButton(StaticObject):
         self.rect.center = (x,y)
 
         self.text = text
-
-    def check_click(self):
+    
+    def check_click(self, mouse: Mouse):
         """
         sets action variable to False\n
         checks the mouse position,\n
@@ -233,22 +216,22 @@ class TextButton(StaticObject):
             write_on_surface(self.surf, self.text, 0, 0, 28, (250, 250, 250), True)
 
             # check if the mouse is clicked
-            if self.game.mouse.click():
+            if mouse.click():
                 action = True
                 # return action
         elif not self.rect.collidepoint(pos):
             self.surf.fill((30, 30, 30))
             write_on_surface(self.surf, self.text, 0, 0, 28, (200, 200, 200), True)
         return action
-
-    def draw(self):
+    
+    def draw(self, screen: pygame.Surface):
         """
         Blits the button surface on the screen,
         draws the border of the button,
         writes the text of the button
         :return:
         """
-        self.game.screen.blit(self.surf, (self.x - self.width / 2, self.y - self.height / 2))
-        pygame.draw.rect(self.game.screen, (250, 250, 250), self.rect, 1)
+        screen.blit(self.surf, (self.x - self.width / 2, self.y - self.height / 2))
+        pygame.draw.rect(screen, (250, 250, 250), self.rect, 1)
         write_on_surface(self.surf, self.text, 0, 0, 28, (200, 200, 200), True)
 
