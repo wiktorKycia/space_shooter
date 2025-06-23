b5f7ed22a5f4f8004a298247c41210d2f14bcfc3
Wiktor Kycia <wiktor.kycia@gmail.com>
2025-06-23 11:24:22 +0200

    paste code from the template

--- main.py
+++ main.py
@@ -26,43 +26,114 @@ tps_max = 100.0
 tps_clock = pygame.time.Clock()
 dt = 0.0
 
-running: bool = True
 
-# objects
-player = Player()
+click: bool = False
 
-shipBuilder = PlayableShipBuilder()
-shipDirector = PlayableShipBuilderDirector(shipBuilder, "Ship1")
+def main_menu():
+	global tps_clock
+	global click
+	global dt
+	running: bool = True
 
-with open("./gameData/playerShips.json") as f:
-	for ship in json.load(f)['ships']:
-		shipDirector.choose_ship(ship['name'])
-		player.add_new_ship(shipDirector.build(width / 2, height / 2))
+	# objects
+	# player = Player()
+	#
+	# shipBuilder = PlayableShipBuilder()
+	# shipDirector = PlayableShipBuilderDirector(shipBuilder, "Ship1")
+	#
+	# with open("./gameData/playerShips.json") as f:
+	# 	for ship in json.load(f)['ships']:
+	# 		shipDirector.choose_ship(ship['name'])
+	# 		player.add_new_ship(shipDirector.build(width / 2, height / 2))
+	#
+	# mouse = Mouse()
+	#
+	# waveManager: WaveManager = WaveManager("./gameData/levels.json")
+	# levelManager: LevelManager = LevelManager(waveManager)
 
-mouse = Mouse()
+	# main loop
+	while running:
+		# clock
+		dt:float = tps_clock.get_time() / 1000
 
-waveManager: WaveManager = WaveManager("./gameData/levels.json")
-levelManager: LevelManager = LevelManager(waveManager)
+		# Draw the background
+		screen.fill((0, 0, 0))
 
-# main loop
-while running:
-	# Events
-	for event in pygame.event.get():
-		if event.type == pygame.QUIT:
-			running = False
+		draw_text('main menu', font, (255, 255, 255), screen, 20, 20)
 
-	# clock
-	dt = tps_clock.get_time() / 1000
-	tps_clock.tick(tps_max)
+		mx, my = pygame.mouse.get_pos()
 
-	# Draw the background
-	screen.fill((0, 0, 0))
+		button_1 = pygame.Rect(50, 100, 200, 50)
+		button_2 = pygame.Rect(50, 200, 200, 50)
+		if button_1.collidepoint((mx, my)):
+			if click:
+				game()
+		if button_2.collidepoint((mx, my)):
+			if click:
+				options()
+		pygame.draw.rect(screen, (255, 0, 0), button_1)
+		pygame.draw.rect(screen, (255, 0, 0), button_2)
 
-	# logic here
+		click = False
 
-	# Do the next tick
-	pygame.display.update()
+		# Events
+		for event in pygame.event.get():
+			if event.type == QUIT:
+				pygame.quit()
+				sys.exit()
+			if event.type == KEYDOWN:
+				if event.key == K_ESCAPE:
+					pygame.quit()
+					sys.exit()
+			if event.type == MOUSEBUTTONDOWN:
+				if event.button == 1:
+					click = True
 
-# Exit
-pygame.quit()
-quit()
+		# Do the next tick
+		pygame.display.update()
+		tps_clock.tick(tps_max)
+
+	# Exit
+	pygame.quit()
+	quit()
+
+
+def game():
+	global tps_clock
+	running = True
+	while running:
+		screen.fill((0, 0, 0))
+
+		draw_text('game', font, (255, 255, 255), screen, 20, 20)
+		for event in pygame.event.get():
+			if event.type == QUIT:
+				pygame.quit()
+				sys.exit()
+			if event.type == KEYDOWN:
+				if event.key == K_ESCAPE:
+					running = False
+
+		pygame.display.update()
+		tps_clock.tick(60)
+
+
+def options():
+	running = True
+	while running:
+		screen.fill((0, 0, 0))
+
+		draw_text('options', font, (255, 255, 255), screen, 20, 20)
+		for event in pygame.event.get():
+			if event.type == QUIT:
+				pygame.quit()
+				sys.exit()
+			if event.type == KEYDOWN:
+				if event.key == K_ESCAPE:
+					running = False
+
+		pygame.display.update()
+		tps_clock.tick(60)
+
+
+if __name__ == "__main__":
+	main_menu()
